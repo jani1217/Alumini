9b4d7b67f513afcb407176e3f9cdb6ba7c7ddde2
jani1217 <muthuyy11@gmail.com>
2025-05-23 13:25:22 +0530

    Initial push of ReactJS project

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -1,13 +1,19 @@
 import './Departments.css';
 
 const departments = [
-  "Computer Science", "Electronics & Communication", "Electrical & Electronics",
-  "Mechanical", "Civil", "Computer Applications", "Mathematics", "Science & Humanities"
+  "Computer Science", 
+  "Electronics & Communication", 
+  "Electrical & Electronics",
+  "Mechanical", 
+  "Civil", 
+  "Computer Applications", 
+  "Mathematics", 
+  "Science & Humanities"
 ];
 
 const Departments = () => (
   <div className="departments">
-    <h1>DEPARTMENTS</h1>
+    <h1 style={{ textAlign: 'center', marginBottom: '1rem' }}>DEPARTMENTS</h1>
     <div className="dept-grid">
       {departments.map((dept, i) => (
         <button key={i}>{dept}</button>
